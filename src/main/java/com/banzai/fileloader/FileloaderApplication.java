b0607657d02f7b3edbdbdb54258553376e0ff243
Diallo Dinn <d.diallo@maximatelecom.ru>
2018-06-19 13:32:22 +0300

    Init

---
+++ src/main/java/com/banzai/fileloader/FileloaderApplication.java
@@ -0,0 +1,12 @@
+package com.banzai.fileloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileloaderApplication.class, args);
+	}
+}
---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,27 @@
+--Ranks
+CREATE TABLE contents (
+  id           BIGINT NOT NULL,
+  content      VARCHAR(1024),
+  creationDate TIMESTAMP WITH TIME ZONE
+);
+
+CREATE SEQUENCE contents_id_seq
+START WITH 1
+INCREMENT BY 1
+NO MINVALUE
+NO MAXVALUE
+CACHE 1;
+
+ALTER SEQUENCE contents_id_seq OWNED BY contents.id;
+ALTER TABLE ONLY contents
+  ALTER COLUMN id SET DEFAULT nextval('contents_id_seq' :: REGCLASS);
+
+ALTER TABLE ONLY contents
+  ADD CONSTRAINT contents_pkey PRIMARY KEY (id);
+
+
+
+
+
+
+
